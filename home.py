299a1cdec0831460979dfaad5b04680efd41e4e9
Hamdan Mohammed Ridwan <hamdanmridwan@outlook.com>
2018-02-27 04:38:22 +0530

    Fixed HTML rendering bugs in flask

--- home.py
+++ home.py
@@ -1,15 +1,9 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template
 app = Flask(__name__)
-import cv2
-import time
-
-def p(i):
-    return "Hello World "+str(i)
-
-
 
 @app.route("/")
-def hello():
-    app.send_static_file('index.html')
-if __name__ == '__main__':  
-  app.run()        
+def index():
+   return render_template("index.html")
+
+if __name__ == '__main__':
+   app.run(debug = True)
